5edc7749d20eb21aa6df08761a388c5fe7901249
Holger Mueller <9jholger@gmail.com>
2022-01-09 19:39:08 -0600

    more work

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let tooltip = d3
+  .select("chart-holder")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("opacity", 0);
+
 d3.json(
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 ).then((data) => {
